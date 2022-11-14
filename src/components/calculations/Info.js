615d1dddeb58ef76b27c06d7b963b951b378caf8
Patrick Hull <patrick9hull4@gmail.com>
2022-11-13 16:28:29 -0800

    feat: small changes

--- src/components/calculations/Info.js
+++ src/components/calculations/Info.js
@@ -4,7 +4,7 @@ import './Info.css'
 export default function Info() {
   return (
     <div className='info-container'>
-      <h2 className='info'>Info</h2>
+      <h2 className='info'>Guide</h2>
       <ul className='info-list'>
         <li>The Food and Drug Administration (FDA) recommends 1,000 IUs of Vitamin D daily,
           which is equivalent to 10-15 minutes of mid-day sun exposure</li>
